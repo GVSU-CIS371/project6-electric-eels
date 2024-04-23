778fb28a0932e066de31949a9ea870f41a452af5
zeratini <gritterb@mail.gvsu.edu>
2024-04-23 02:46:44 +0000

    Base firebase interaction ts files

---
+++ src/deleteFuncs.ts
@@ -0,0 +1,13 @@
+import {
+    collection,
+    deleteDoc,
+    deleteField,
+    doc,
+    Firestore,
+    getDocs,
+    query,
+    QueryDocumentSnapshot,
+    QuerySnapshot,
+    updateDoc,
+    where,
+  } from "firebase/firestore";
---
+++ src/updateFuncs.ts
@@ -0,0 +1,14 @@
+import {
+    collection,
+    deleteDoc,
+    deleteField,
+    doc,
+    Firestore,
+    getDocs,
+    query,
+    QueryDocumentSnapshot,
+    QuerySnapshot,
+    updateDoc,
+    where,
+  } from "firebase/firestore";
+  
